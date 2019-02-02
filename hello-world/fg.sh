d7ee6324c3761e7c92f1e139dacc7cf749b0276e
Alex OpenSource <7654306+h4v0kr@users.noreply.github.com>
2019-02-02 11:36:05 +0200

    Update fg.sh

--- hello-world/fg.sh
+++ hello-world/fg.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "Hello world"
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
