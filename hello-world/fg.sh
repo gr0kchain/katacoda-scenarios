22dbef290c47901a39649ed757953190c5fd180a
Alex Griessel <alex.griessel@gmail.com>
2019-02-05 19:47:00 +0200

    Added Loader

--- hello-world/fg.sh
+++ hello-world/fg.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-i=1
-sp="/-\|"
-echo -n ' '
-while [ $(docker ps | wc -l) -eq 1 ];
-do
-    printf "\b${sp:i++%${#sp}:1}"
-    sleep 1
-done
-docker exec -it  bitcoind-node /bin/bash
+sleep 1; wait.sh
