4edccaba0b01dedff3151172b3bf2f1a853a5dc6
Alex Griessel <alex.griessel@gmail.com>
2019-02-05 20:46:03 +0200

    testing

--- hello-world/foreground.sh
+++ hello-world/foreground.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sleep 1; loading.sh
+i=1
+sp="/-\|"
+echo -n ' '
+while [ $(docker ps | wc -l) -eq 1 ];
+do
+    printf "\b${sp:i++%${#sp}:1}"
+    sleep 1
+done
+docker exec -it  bitcoind-node /bin/bash
